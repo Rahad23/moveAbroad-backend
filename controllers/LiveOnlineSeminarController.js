cdbd6eeb0c01113a94eeaccab64dce4dab968454
Rahad23 <rahadhasan33675@gmail.com>
2023-07-17 20:41:20 +0600

    daynamic live seminar

--- controllers/LiveOnlineSeminarController.js
+++ controllers/LiveOnlineSeminarController.js
@@ -1,8 +1,80 @@
 const { liveOnlineSeminarCollection } = require("../mongoDBConfig/collections");
 const path = require('path');
-const { createDoc, deleteDoc } = require("../utils/mongoQuery");
+const { createDoc, deleteDoc, updateDoc } = require("../utils/mongoQuery");
+const { ObjectId } = require("mongodb");
 const imageFolderPath = path.join(`${process.env.APP_IMG_URL}/Assets/UniversityImg`);
 
+
+
+// const getLiveOnlineSeminar = async (req, res) => {
+//   try {
+//     const booksData = await liveOnlineSeminarCollection().find({}).toArray();
+//     const seminarsWithCountdown = booksData.map(async (liveSeminarData) => {
+//       const currentDate = new Date();
+//       const registrationPublish = liveSeminarData.publishDate;
+//       const registrationTime = liveSeminarData.registrationTiming;
+//       const publishDate = new Date(registrationPublish); // Corrected publishDate
+//       const registrationTimeLimitMonths = new Date(registrationTime); // Registration time limit in months (adjust as needed)
+
+//       // Calculate the time difference in milliseconds
+//       const timeDifference = registrationTimeLimitMonths - currentDate;
+
+//       // Check if the registration time has passed
+//       if (timeDifference <= 0) {
+//         // Registration time has passed, set all countdown values to 0
+//         return {
+//           ...liveSeminarData,
+//           imagePath: `${imageFolderPath}/${liveSeminarData.universityImg}`,
+//           countdown: {
+//             years: 0,
+//             months: 0,
+//             days: 0,
+//             hours: 0,
+//             minutes: 0,
+//             seconds: 0,
+//           },
+//         };
+//       }
+
+//       // Convert the time difference to days, months, and years
+//       const millisecondsInADay = 1000 * 60 * 60 * 24;
+//       const daysDifference = Math.floor(timeDifference / millisecondsInADay);
+//       const monthsDifference = Math.floor(daysDifference / 30); // Assuming 30 days in a month
+//       const remainingDays = daysDifference % 30;
+
+//       // Calculate the years and months
+//       const yearsDifference = Math.floor(monthsDifference / 12);
+//       const monthsRemaining = monthsDifference % 12;
+
+//       // Calculate remaining hours, minutes, and seconds
+//       const hoursDifference = Math.floor((timeDifference / (1000 * 60 * 60)) % 24);
+//       const minutesDifference = Math.floor((timeDifference / (1000 * 60)) % 60);
+//       const secondsDifference = Math.floor((timeDifference / 1000) % 60);
+
+//       // Prepare the result object with the countdown information
+//       const liveSeminarWithCountdown = {
+//         ...liveSeminarData,
+//         imagePath: `${imageFolderPath}/${liveSeminarData.universityImg}`,
+//         countdown: {
+//           years: yearsDifference,
+//           months: monthsRemaining,
+//           days: remainingDays,
+//           hours: hoursDifference,
+//           minutes: minutesDifference,
+//           seconds: secondsDifference,
+//         },
+//       };
+//       return liveSeminarWithCountdown;
+//     });
+
+//     // Wait for all the countdown calculations to complete
+//     const result = await Promise.all(seminarsWithCountdown);
+//     res.json(result);
+//   } catch (error) {
+//     console.error('Error fetching live online seminar data:', error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// };
 
 
 
@@ -18,6 +90,23 @@ const getLiveOnlineSeminar = async (req, res) => {
 
       // Calculate the time difference in milliseconds
       const timeDifference = registrationTimeLimitMonths - currentDate;
+
+ // Check if the registration time has passed
+ if (timeDifference <= 0) {
+  // Registration time has passed, set all countdown values to 0
+  return {
+    ...liveSeminarData,
+    imagePath: `${imageFolderPath}/${liveSeminarData.universityImg}`,
+    countdown: {
+      years: 0,
+      months: 0,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    },
+  };
+}
 
       // Convert the time difference to days, months, and years
       const millisecondsInADay = 1000 * 60 * 60 * 24;
@@ -52,6 +141,7 @@ const getLiveOnlineSeminar = async (req, res) => {
 
     // Wait for all the countdown calculations to complete
     const result = await Promise.all(seminarsWithCountdown);
+    console.log(".")
     res.json(result);
   } catch (error) {
     console.error('Error fetching live online seminar data:', error);
@@ -59,10 +149,29 @@ const getLiveOnlineSeminar = async (req, res) => {
   }
 };
 
+const getOneLiveSeminarData= async(req, res)=>{
+    const dataOneBook = await liveOnlineSeminarCollection().findOne({
+      _id: new ObjectId(req.params.id)
+  })
+  const liveSeminarWithImagePaths = {
+      ...dataOneBook,
+      imagePath: `${imageFolderPath}`,
+    };
+
+  res.send({
+      ...liveSeminarWithImagePaths,
+  } || {})
+}
+
 const saveLiveOnlineSeminar = async (req, res) => {
     const result = await createDoc(req, liveOnlineSeminarCollection);
     res.send(result);
 };
+
+const updateLiveOnlineSeminar = async(req, res)=>{
+  const result = await updateDoc(req, liveOnlineSeminarCollection);
+  res.send(result);
+}
 
 const deleteLiveOnlineSeminar= async(req, res)=>{
   const result = await deleteDoc(req, liveOnlineSeminarCollection);
@@ -72,6 +181,8 @@ const deleteLiveOnlineSeminar= async(req, res)=>{
 
 module.exports = {
     getLiveOnlineSeminar,
+    getOneLiveSeminarData,
     saveLiveOnlineSeminar,
+    updateLiveOnlineSeminar,
     deleteLiveOnlineSeminar
 }
--- routes/LiveOnlineSeminar.js
+++ routes/LiveOnlineSeminar.js
@@ -1,9 +1,11 @@
-const { getLiveOnlineSeminar, saveLiveOnlineSeminar, deleteLiveOnlineSeminar } = require("../controllers/LiveOnlineSeminarController");
+const { getLiveOnlineSeminar, saveLiveOnlineSeminar, deleteLiveOnlineSeminar, updateLiveOnlineSeminar, getOneLiveSeminarData } = require("../controllers/LiveOnlineSeminarController");
 
 const liveOnlineSeminar = require("express").Router();
 
 liveOnlineSeminar.get("/", getLiveOnlineSeminar);
+liveOnlineSeminar.get("/:id", getOneLiveSeminarData)
 liveOnlineSeminar.post("/", saveLiveOnlineSeminar);
+liveOnlineSeminar.patch('/:id', updateLiveOnlineSeminar)
 liveOnlineSeminar.delete("/:id", deleteLiveOnlineSeminar)
 
 module.exports = {
